85e2caa1b91ed9ae8186b422850ddc508c1d6eec
Saiful082 <smsaifulislam082@gmail.com>
2023-06-23 01:11:15 +0600

    login from  product addded

--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -1,5 +1,8 @@
 import Main from "../../Layout/Main";
 import Home from "../../Pages/Home/Home/Home";
+import Login from "../../Pages/Login/Login";
+import Register from "../../Pages/Register/Register";
+
 
 const { createBrowserRouter } = require("react-router-dom");
 
@@ -11,6 +14,14 @@ const router = createBrowserRouter([
         {
             path: '/',
             element: <Home></Home>
+        },
+        {
+          path: '/login',
+          element: <Login></Login>
+        },
+        {
+          path: '/register',
+          element: <Register></Register>
         }
       ]
     }
