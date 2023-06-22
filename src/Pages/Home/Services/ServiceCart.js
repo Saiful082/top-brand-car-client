ed5fdb18cbe2ff69af6d36727ec9d690d0855774
Saiful082 <smsaifulislam082@gmail.com>
2023-06-22 19:52:23 +0600

    service product addded

--- src/Pages/Home/Services/ServiceCart.js
+++ src/Pages/Home/Services/ServiceCart.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-const ServiceCart = () => {
+
+const ServiceCart = ({service}) => {
+    const {img, price, title} = service;
     return (
         <div className="card card-compact w-96 bg-base-100 shadow-xl">
-            <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+            <figure><img src={img} alt="Shoes" /></figure>
             <div className="card-body">
-                <h2 className="card-title">Shoes!</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <h2 className="card-title">{title}</h2>
+                <p className='text-2xl text-orange-600 font-semibold'>Price: ${price}</p>
                 <div className="card-actions justify-end">
                     <button className="btn btn-primary">Buy Now</button>
                 </div>
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -12,16 +12,16 @@ const Services = () => {
 
     return (
         <div>
-            <div className='text-center'>
+            <div className='text-center mb-4'>
                 <p className='text-2xl font-blod text-orange-600'>Service</p>           
                 <h2 className='text-5xl font-semibold'>Our Service Area</h2>
                 <p>The majority have suffered alteration in some form, by injected humour.</p>
             </div>
-            <div className=''>
+            <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
                 {
                     services.map(service => <ServiceCart 
                         key={service._id}
-
+                        service={service}
                         ></ServiceCart>)
                 }
             </div>
